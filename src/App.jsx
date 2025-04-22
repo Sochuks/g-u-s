2a3e6e9eebb7c4c513f8c730df1b60b31e4823bf
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2025-04-22 21:13:49 +0100

    Initialized project dir and install dep

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+    <div className="min-h-screen bg-gray-200 flex items-center justify-center">
+      <h1 className="text-2xl font-bold">GitHub User Search</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
